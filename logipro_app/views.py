844dfad10af28c2bd19e625a5a91ac6c7e9ccf98
[Nilay Purayar] <[purayar.nilay0611@gmail.com]>
2022-12-01 13:52:37 +0530

    project initialized

---
+++ logipro_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+# from .models import TodoListItem 
+from django.http import HttpResponseRedirect 
+
+from django.contrib.auth.decorators import login_required
+
+def index(request):
+    return render(request, 'index.html')
