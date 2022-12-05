b8ca2a8522daec04968f46877b807674d81eb830
[Nilay Purayar] <[purayar.nilay0611@gmail.com]>
2022-12-05 14:50:54 +0530

    catchup

--- logipro_app/models.py
+++ logipro_app/models.py
@@ -5,3 +5,13 @@ from django.db import models
 class logiProItems(models.Model):
     user = models.CharField(max_length=100)
     
+class RegItems(models.Model):
+    sno= models.AutoField(primary_key=True)  
+    venue=models.CharField(max_length=100)
+    event_Date=models.DateField()
+    micw=models.IntegerField()
+    micwl=models.IntegerField()
+    speakers=models.IntegerField()
+    light=models.IntegerField()
+    chair=models.IntegerField()
+    Table=models.IntegerField()
