fcb4bc7fba7718243f3919bfb5311ad0f11904dd
[Nilay Purayar] <[purayar.nilay0611@gmail.com]>
2022-12-05 16:40:13 +0530

    Login Error

--- logipro_app/urls.py
+++ logipro_app/urls.py
@@ -3,6 +3,5 @@ from . import views
 app_name = 'logipro'
 urlpatterns = [
     path('',views.login, name="login"),
-    path('logipro/', views.login_required, name='logipro'),
-    path('details/',views.logipro_appView,name="details"),
+    path('basicDetails/', views.logipro_appView, name='basicDetails'),
 ] 
